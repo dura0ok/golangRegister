523fe6954172f39ce8bd3a7f00bd39cac606a8d0
stepan7 <fenixrnd@mail.ru>
2020-02-28 22:09:02 +0300

    start training

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type User struct {
+	Email string
+	Password string
+}
+
+func checkRequestMethod(req http.Request, need string) bool {
+	if req.Method != need{
+		return false
+	}
+	return true
+}
+
+func register(w http.ResponseWriter, req *http.Request){
+	if checkRequestMethod(*req, "POST"){
+
+		var person User
+
+		// Try to decode the request body into the struct. If there is an error,
+		// respond to the client with the error message and a 400 status code.
+		err := json.NewDecoder(req.Body).Decode(&person)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		//TODO Register User
+		//Здесь буду регать юзера
+
+
+	}else{
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, err := w.Write([]byte("This route must run only by POST request"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+
+func main() {
+
+	http.HandleFunc("/register", register)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+
+}
